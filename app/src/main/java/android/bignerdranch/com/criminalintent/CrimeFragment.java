bfd2b70b19e131759c51b463e8dd8a9074dbe44f
Chris L. <chris.lichter@gmail.com>
2014-12-04 17:18:07 -0600

    Update CrimeFragment.java

--- app/src/main/java/android/bignerdranch/com/criminalintent/CrimeFragment.java
+++ app/src/main/java/android/bignerdranch/com/criminalintent/CrimeFragment.java
@@ -35,6 +35,7 @@ public class CrimeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
         
         mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             public void onTextChanged(
                 CharSequence c, int start, int before, int count){
@@ -60,6 +61,7 @@ public class CrimeFragment extends Fragment {
         mDateButton.setEnabled(false);
         
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
