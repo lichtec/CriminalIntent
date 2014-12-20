1b57d1aa1eabc92a87aeda57535964a5ea3ac3bd
Chris L <chris.lichter@gmail.com>
2014-12-20 15:07:50 -0600

    Adding DialogFragment

---
+++ app/src/main/java/android/bignerdranch/com/criminalintent/DatePickerFragment.java
@@ -0,0 +1,11 @@
+package android.bignerdranch.com.criminalintent;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by christopherl on 12/20/14.
+ */
+public class DatePickerFragment extends DialogFragment {
+    
+
+}
