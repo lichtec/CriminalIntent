2a46f71233f2c4b750504292f52b424da1b7b0c0
Chris L. <chris.lichter@gmail.com>
2014-12-23 16:17:10 -0600

    Update DatePickerFragment.java

--- app/src/main/java/android/bignerdranch/com/criminalintent/DatePickerFragment.java
+++ app/src/main/java/android/bignerdranch/com/criminalintent/DatePickerFragment.java
@@ -7,5 +7,14 @@ import android.support.v4.app.DialogFragment;
  */
 public class DatePickerFragment extends DialogFragment {
     
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+        
+        return new AlterDialog.Builder(getActivity()).setView(v)
+        .setTitle(R.string.date_picker_title).setPositiveButton(android.R.string.ok, null)
+        .create();
+    }
+    
 
 }
